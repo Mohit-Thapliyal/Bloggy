b10c3343915ad7751dbfe3bafc535bcdb8226760
Mohit Thapliyal <mohitthapliyal100@gmail.com>
2023-05-31 00:02:11 +0530

    cors resolved

--- pages/api/blog/[id].ts
+++ pages/api/blog/[id].ts
@@ -2,15 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { ResponseFuncs } from "../../../utils/types";
 import { connect } from "@/utils/connectDB";
 import Blog from "@/models/blog";
-import NextCors from "nextjs-cors";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // await NextCors(req, res, {
-  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  //   origin: "https://bloggy-mohit-thapliyal.vercel.app",
-  //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // });
-
   //capture request method, we type it as a key of ResponseFunc to reduce typing later
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
 
@@ -42,7 +35,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   // Check if there is a response for the particular method, if so invoke it, if not response with an error
-  res.setHeader("Access-Control-Allow-Origin", "https://bloggy-mohit-thapliyal.vercel.app")
   const response = handleCase[method];
   if (response) response(req, res);
   else res.status(400).json({ error: "No Response for This Request" });
--- pages/api/blog/index.ts
+++ pages/api/blog/index.ts
@@ -1,40 +1,33 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { ResponseFuncs } from "../../../utils/types";
-import { connect } from "@/utils/connectDB";
-import Blog from "@/models/blog";
-import NextCors from "nextjs-cors";
+import { NextApiRequest, NextApiResponse } from "next"
+import { ResponseFuncs } from "../../../utils/types"
+import { connect } from "@/utils/connectDB"
+import Blog from "@/models/blog"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // await NextCors(req, res, {
-  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  //   origin: "https://bloggy-mohit-thapliyal.vercel.app",
-  //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // });
-
   //capture request method, we type it as a key of ResponseFunc to reduce typing later
-  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
+  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
 
   //function for catch errors
-  const catcher = (error: Error) => res.status(400).json({ error });
+  const catcher = (error: Error) => res.status(400).json({ error })
 
   // Potential Responses
   const handleCase: ResponseFuncs = {
     // RESPONSE FOR GET REQUESTS
     GET: async (req: NextApiRequest, res: NextApiResponse) => {
-      await connect(); // connect to database
-      res.json(await Blog.find().catch(catcher));
+      await connect() // connect to database
+      res.json(await Blog.find().catch(catcher))
     },
     // RESPONSE POST REQUESTS
     POST: async (req: NextApiRequest, res: NextApiResponse) => {
-      await connect(); // connect to database
-      res.json(await Blog.create(req.body).catch(catcher));
+      await connect() // connect to database
+      res.json(await Blog.create(req.body).catch(catcher)) 
     },
-  };
+  }
 
   // Check if there is a response for the particular method, if so invoke it, if not response with an error
-  const response = handleCase[method];
-  if (response) response(req, res);
-  else res.status(400).json({ error: "No Response for This Request" });
-};
+  const response = handleCase[method]
+  if (response) response(req, res)
+  else res.status(400).json({ error: "No Response for This Request" })
+}
 
-export default handler;
+export default handler
