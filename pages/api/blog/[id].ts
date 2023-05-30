3fc72a68af7a1c883b2f7a68478ac85aa1bff87f
Mohit Thapliyal <mohitthapliyal100@gmail.com>
2023-05-30 23:31:09 +0530

    allowed origin

--- pages/api/blog/[id].ts
+++ pages/api/blog/[id].ts
@@ -7,7 +7,7 @@ import NextCors from "nextjs-cors";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
+    origin: "https://bloggy-mohit-thapliyal.vercel.app",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
